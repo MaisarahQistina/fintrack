db6aa7ffe5053f0462b6534148c89a4be4b3798b
MaisarahQistina <maisarahtina@gmail.com>
2025-02-26 12:33:41 +0800

    update infographic page title

--- src/Infographics/UserInfographicView.js
+++ src/Infographics/UserInfographicView.js
@@ -5,7 +5,7 @@ import CardSlider from "./CardSlider";
 function UserInfographicView() {
   return (
     <main className={styles.userInfographicView}> 
-      <h2 className={styles.heading}>Boost Your Financial Literacy !</h2>
+      <h2 className={styles.heading}>Infographics - Boost Your Financial Literacy !</h2>
       <CardSlider />
     </main>
   );
