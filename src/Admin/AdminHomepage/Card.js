4b5f2305c78957b7d05f19a6779e21238e15ca2b
MaisarahQistina <maisarahtina@gmail.com>
2025-02-28 15:45:32 +0800

    admin home - button link to page

--- src/Admin/AdminHomepage/Card.js
+++ src/Admin/AdminHomepage/Card.js
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./AdminHome.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Card = ({ imageUrl, imageAlt, buttonText }) => {
+const Card = ({ imageUrl, imageAlt, buttonText, link }) => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.cardWrapper}>
       <article className={styles.card}>
         <img src={imageUrl} alt={imageAlt} className={styles.cardImage} />
       </article>
-      <button className={styles.cardButton}>{buttonText}</button>
+      <button className={styles.cardButton} onClick={() => navigate(link)}>
+        {buttonText}
+      </button>
     </div>
   );
 };
 
 export default Card;
+
--- src/Admin/AdminHomepage/CardsContainer.js
+++ src/Admin/AdminHomepage/CardsContainer.js
@@ -9,18 +9,21 @@ const CardsContainer = () => {
         "https://cdn.builder.io/api/v1/image/assets/TEMP/92c082d118f56d989717e7052bcf65f649ed6ddb",
       imageAlt: "Tax Relief illustration",
       buttonText: "Tax Relief",
+      link: "/tax-relief", // Placeholder route
     },
     {
       imageUrl:
         "https://cdn.builder.io/api/v1/image/assets/TEMP/e72b380da57f5f6e28d258ce1ea1f20092ea5800",
       imageAlt: "Infographics illustration",
       buttonText: "Infographics",
+      link: "/infographics", // Placeholder route
     },
     {
       imageUrl:
         "https://cdn.builder.io/api/v1/image/assets/TEMP/7aee8ee68f67727a42995abdcb54f4c51b8f3287",
       imageAlt: "Feedbacks illustration",
       buttonText: "Feedbacks",
+      link: "/admin-feedback", 
     },
   ];
 
@@ -32,6 +35,7 @@ const CardsContainer = () => {
           imageUrl={card.imageUrl}
           imageAlt={card.imageAlt}
           buttonText={card.buttonText}
+          link={card.link}
         />
       ))}
     </section>
