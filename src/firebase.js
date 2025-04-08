9f1a415383ea36f49d1fdb2d6306a81f4349b107
MaisarahQistina <maisarahtina@gmail.com>
2025-04-08 08:05:30 +0800

    update db

--- src/firebase.js
+++ src/firebase.js
@@ -15,8 +15,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
